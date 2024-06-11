6c0d62f35ce8f4a3dc608dbb685aadc539d2d008
Frantz Chery <frantzchery@hotmail.com>
2024-06-11 09:30:18 -0400

    first commit

---
+++ models/Tweet.js
@@ -0,0 +1,45 @@
+import mongoose, { mongo } from 'mongoose';
+import { commentSchema } from './Comments.js';
+
+// const tweetDoc = {
+//       content: newTweet,
+//       username: 'abe123',
+//       likes: 0,
+//       retweets: 0,
+//       timestamp: new Date(),
+//       id: uuidv4(),
+//     };
+
+const tweetSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+
+    comments: [commentSchema], // Nested document
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    retweets: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Tweet', tweetSchema);
